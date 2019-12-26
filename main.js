f64fce451b168a11dc89e11d887fbc3b96b6df46
MapGIS <9409101+MapGIS-Curran@users.noreply.github.com>
2019-12-26 02:03:13 -0900

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-require(["esri/Map", "esri/views/SceneView"], function(Map, SceneView) {
-        var map = new Map({
-          basemap: "streets",
-          ground: "world-elevation"
-        });
+require(["esri/Map", "esri/views/SceneView"], 
+function(Map, SceneView) 
+ {
+  var map = new Map({
+    basemap: "streets",
+    ground: "world-elevation"
+  });
 
-        var view = new SceneView({
-          container: "viewDiv",
-          map: map,
-          scale: 4000000,
-          center: [-95, 40]
-        });
-      });
+  var view = new SceneView({
+    container: "viewDiv",
+    map: map,
+    scale: 4000000,
+    center: [-95, 40]
+  });
+});
